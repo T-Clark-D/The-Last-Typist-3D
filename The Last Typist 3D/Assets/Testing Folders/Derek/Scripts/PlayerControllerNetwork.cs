9bc19c3fa41fa81075abea1a5912b8b007241385
Djzx2010 <dereklamtsangon@gmail.com>
2022-03-11 19:13:38 -0500

    each player can text, but does not show to everyone

--- The Last Typist 3D/Assets/Testing Folders/Derek/Scripts/PlayerControllerNetwork.cs
+++ The Last Typist 3D/Assets/Testing Folders/Derek/Scripts/PlayerControllerNetwork.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 
 public class PlayerControllerNetwork : MonoBehaviour
 {
     public float m_speed;
+
+    private bool m_isCombatMode;
+
     private Rigidbody m_RB;
+    public GameObject m_writtenTextBox;
+    public Text m_writtenText;
 
     private PhotonView view;
 
@@ -15,13 +21,42 @@ public class PlayerControllerNetwork : MonoBehaviour
     {
         m_RB = GetComponent<Rigidbody>();
         view = GetComponent<PhotonView>();
+        m_writtenText = m_writtenTextBox.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (view.IsMine)
+        {
+            if (m_isCombatMode)
+            {
+                GetTextInput();
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                m_isCombatMode = !m_isCombatMode;
+            }
+        }
     }
 
     void FixedUpdate()
     {
         if (view.IsMine)
         {
-            Movement();
+            if (!m_isCombatMode)
+            {
+                Movement();
+            }
+        }
+    }
+
+    private void GetTextInput()
+    {
+        m_writtenText.text += Input.inputString;
+        if (Input.GetKeyDown(KeyCode.Backspace) && m_writtenText.text.Length > 1)
+        {
+            string backSpacedText = m_writtenText.text.Substring(0, m_writtenText.text.Length - 2);
+            m_writtenText.text = backSpacedText;
         }
     }
 
