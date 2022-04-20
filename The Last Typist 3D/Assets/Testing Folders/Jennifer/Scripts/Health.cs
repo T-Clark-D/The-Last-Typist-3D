46963ec5779f963fa92cd689d0d506bbd460a793
Jenni-Gaming <68715746+Jenni-Gaming@users.noreply.github.com>
2022-04-19 23:12:02 -0400

    Added GameOverUI

--- The Last Typist 3D/Assets/Testing Folders/Jennifer/Scripts/Health.cs
+++ The Last Typist 3D/Assets/Testing Folders/Jennifer/Scripts/Health.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
@@ -19,6 +20,7 @@ public class Health : MonoBehaviour
         {
             health = numOfHearts;
         }
+
         for(int i = 0; i < hearts.Length; i++)
         {
             if (i < health)
@@ -37,6 +39,11 @@ public class Health : MonoBehaviour
             }
             else
                 hearts[i].enabled = false;
+        }
+
+        if(health == 0)
+        {
+            SceneManager.LoadScene("GameOverScene");
         }
     }
 
